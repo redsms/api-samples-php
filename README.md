31c9a044a4657787cb9eab81c0541695f122bc33
shatilov <shatilov@resms.ru>
2017-10-26 11:29:59 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**Simple api wrapper for https://cp.redsms.ru service**
+
+For use the service you need to register and specify api-key in the settings
+https://cp.redsms.ru/settings/
+
--- sample.php
+++ sample.php
@@ -4,8 +4,9 @@ require 'Redsms/RedsmsApiSimple.php';
 
 echo "redsms.ru api test \n";
 
-$login = 'shatilov';
-$apiKey = 'testapikey';
+$login = 'you_login';
+$apiKey = 'you_api_key';
+$testNumber = 'you_number';
 
 $smsApi = new \Redsms\RedsmsApiSimple($login, $apiKey);
 $lastMessageUuid = '';
@@ -15,7 +16,7 @@ try {
     print_r($smsApi->clientInfo());
 
     echo "Send sms message: \n";
-    $sendResult = $smsApi->send('+79057643232', 'Привет это тестовое сообщение!', 'REDSMS.RU');
+    $sendResult = $smsApi->send($testNumber, 'Hello,it is test!', 'REDSMS.RU');
     if ($messages = ($sendResult['items'] ?? [])) {
         foreach ($messages as $message) {
             echo $message['phone'].":".$message['uuid'];
